7771ef92cc7854a64212ec6d8a2d8d0ff75a972b
AnnTumanina <98632330+AnnTumanina@users.noreply.github.com>
2022-05-23 13:56:25 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 @FunctionalInterface
 interface Convert<T,N>{
     N convert(T t);
-    static <T> boolean isNotNull(T t){
-        return  t != null;
-    }
 }
 
 class One{
